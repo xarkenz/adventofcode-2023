efbe2df35c274343d897a3b0b24cb33b40a57ff1
xarkenz <seanedwardsclarke@gmail.com>
2023-12-03 17:47:31 -0600

    cleaned up day01 and day02

--- src/solution/day01.rs
+++ src/solution/day01.rs
@@ -1,14 +1,22 @@
 use super::*;
 
 pub fn run() {
-    let input = get_input("day01.txt");
+    let lines = get_input("day01.txt").lines()
+        .map(expect_line);
+
     let mut calibration_sum = 0;
     let mut real_calibration_sum = 0;
-    for line in input.lines().map(expect_line) {
+
+    for line in lines {
         if let Some(first_digit) = line.bytes().find(u8::is_ascii_digit) {
             let last_digit = line.bytes().rfind(u8::is_ascii_digit).unwrap();
             calibration_sum += (first_digit - b'0') as u32 * 10 + (last_digit - b'0') as u32;
         }
+
+        // After the fact, I was made aware that there's a better solution to the problem of overlapping digits,
+        // which is to replace each digit word while preserving the first and last characters.
+        // That is to say, replace "one" with "o1e", "two" with "t2o", "three" with "t3e", and so on.
+        // Below is the alternate approach I originally used to solve the problem.
         let line = line
             .replace("oneight", "oneeight")
             .replace("twone", "twoone")
@@ -27,11 +35,13 @@ pub fn run() {
             .replace("seven", "7")
             .replace("eight", "8")
             .replace("nine", "9");
+
         if let Some(first_digit) = line.bytes().find(u8::is_ascii_digit) {
             let last_digit = line.bytes().rfind(u8::is_ascii_digit).unwrap();
             real_calibration_sum += (first_digit - b'0') as u32 * 10 + (last_digit - b'0') as u32;
         }
     }
+    
     println!("[01p1] Sum of calibration values: {calibration_sum}");
     println!("[01p2] Real sum of calibration values: {real_calibration_sum}");
 }
--- src/solution/day02.rs
+++ src/solution/day02.rs
@@ -1,25 +1,32 @@
 use super::*;
 
 pub fn run() {
-    let input = get_input("day02.txt");
+    let lines = get_input("day02.txt").lines()
+        .map(expect_line);
+
     let mut total_game_id_sum = 0;
     let mut impossible_game_id_sum = 0;
     let mut power_sum = 0;
-    for line in input.lines().map(expect_line) {
+
+    for line in lines {
         if let Some((game_id, handfuls)) = line.split_once(": ") {
             let game_id = game_id[5..].parse::<u32>()
                 .expect("failed to get game id");
+
             total_game_id_sum += game_id;
+
             let mut is_impossible = false;
             let mut required_red = 0;
             let mut required_green = 0;
             let mut required_blue = 0;
+
             for handful in handfuls.split("; ") {
                 for subset in handful.split(", ") {
                     let (quantity, color) = subset.split_once(' ')
                         .expect("failed to get quantity and color");
                     let quantity = quantity.parse::<u32>()
                         .expect("invalid quantity");
+
                     match color {
                         "red" => {
                             is_impossible = is_impossible || quantity > 12;
@@ -37,13 +44,17 @@ pub fn run() {
                     }
                 }
             }
+
             if is_impossible {
                 impossible_game_id_sum += game_id;
             }
+
             power_sum += required_red * required_green * required_blue;
         }
     }
+
     let possible_game_id_sum = total_game_id_sum - impossible_game_id_sum;
+    
     println!("[02p1] Sum of possible game IDs: {possible_game_id_sum}");
     println!("[02p2] Sum of powers of cube sets: {power_sum}");
 }

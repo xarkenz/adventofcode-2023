3b4fac1188223b9c4765eb48893b1abb999929ef
xarkenz <seanedwardsclarke@gmail.com>
2023-12-03 17:32:58 -0600

    added Map2D, cleaned up day03

--- src/solution/day03.rs
+++ src/solution/day03.rs
@@ -2,66 +2,77 @@ use super::*;
 
 use std::collections::BTreeMap;
 
+fn is_part_symbol(tile: u8) -> bool {
+    tile != b'.' && tile.is_ascii_punctuation()
+}
+
 pub fn run() {
-    let schematic: Vec<Vec<u8>> = get_input("day03.txt").lines().map(expect_line)
-        .map(String::into_bytes)
-        .collect();
-    let width = schematic[0].len();
-    let height = schematic.len();
+    let lines = get_input("day03.txt").lines()
+        .map(expect_line)
+        .map(String::into_bytes);
+    let schematic = Map2D::from_rows(lines, b'.');
+
     let mut part_number_sum = 0;
-    let mut gear_part_number_lists: BTreeMap<(usize, usize), Vec<u32>> = BTreeMap::new();
-    for row in 0..height {
-        let mut col = 0;
-        while col < width {
-            if schematic[row][col].is_ascii_digit() {
+    let mut gear_part_number_lists: BTreeMap<Point2D, Vec<u32>> = BTreeMap::new();
+
+    for y in schematic.min_y() ..= schematic.max_y() {
+        let mut x = schematic.min_x();
+        
+        while x <= schematic.max_x() {
+            if schematic.get(x, y).is_ascii_digit() {
                 let mut number = 0;
-                let mut adjacent_parts: Vec<(usize, usize)> = Vec::new();
-                let mut check_if_adjacent_part = |row: usize, col: usize| {
-                    if schematic[row][col] != b'.' && schematic[row][col].is_ascii_punctuation() {
-                        adjacent_parts.push((row, col));
+                let mut adjacent_parts = Vec::new();
+
+                let mut check_if_adjacent_part = |x, y| {
+                    if is_part_symbol(schematic.get(x, y)) {
+                        adjacent_parts.push(Point2D(x, y));
                     }
                 };
-                if col > 0 {
-                    check_if_adjacent_part(row, col - 1);
-                    if row > 0 { check_if_adjacent_part(row - 1, col - 1); }
-                    if row < height - 1 { check_if_adjacent_part(row + 1, col - 1); }
+                
+                check_if_adjacent_part(x - 1, y - 1);
+                check_if_adjacent_part(x - 1, y);
+                check_if_adjacent_part(x - 1, y + 1);
+
+                while schematic.get(x, y).is_ascii_digit() {
+                    number = number * 10 + (schematic.get(x, y) - b'0') as u32;
+
+                    check_if_adjacent_part(x, y - 1);
+                    check_if_adjacent_part(x, y + 1);
+
+                    x += 1;
                 }
-                while col < width && schematic[row][col].is_ascii_digit() {
-                    number = number * 10 + (schematic[row][col] - b'0') as u32;
-                    if row > 0 { check_if_adjacent_part(row - 1, col); }
-                    if row < height - 1 { check_if_adjacent_part(row + 1, col); }
-                    col += 1;
-                }
-                if col < width {
-                    check_if_adjacent_part(row, col);
-                    if row > 0 { check_if_adjacent_part(row - 1, col); }
-                    if row < height - 1 { check_if_adjacent_part(row + 1, col); }
-                }
+
+                check_if_adjacent_part(x, y - 1);
+                check_if_adjacent_part(x, y);
+                check_if_adjacent_part(x, y + 1);
+
                 if !adjacent_parts.is_empty() {
                     part_number_sum += number;
                 }
-                for (part_row, part_col) in adjacent_parts {
-                    if schematic[part_row][part_col] == b'*' {
-                        if let Some(part_number_list) = gear_part_number_lists.get_mut(&(part_row, part_col)) {
+
+                for part in adjacent_parts {
+                    if schematic.get_at(part) == b'*' {
+                        if let Some(part_number_list) = gear_part_number_lists.get_mut(&part) {
                             part_number_list.push(number);
                         }
                         else {
-                            gear_part_number_lists.insert((part_row, part_col), vec![number]);
+                            gear_part_number_lists.insert(part, vec![number]);
                         }
                     }
                 }
             }
-            else {
-                col += 1;
-            }
+
+            x += 1;
         }
     }
+
     let mut gear_ratio_sum = 0;
     for part_number_list in gear_part_number_lists.values() {
         if part_number_list.len() == 2 {
             gear_ratio_sum += part_number_list[0] * part_number_list[1];
         }
     }
+
     println!("[03p1] Sum of part numbers: {part_number_sum}");
     println!("[03p2] Sum of gear ratios: {gear_ratio_sum}");
 }
--- src/solution/mod.rs
+++ src/solution/mod.rs
@@ -34,3 +34,190 @@ pub fn get_input(name: &'static str) -> std::io::BufReader<std::fs::File> {
 pub fn expect_line(result: std::io::Result<String>) -> String {
     result.expect("error while reading input file")
 }
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Debug)]
+pub struct Point2D(pub i64, pub i64);
+
+impl Point2D {
+    pub fn x(&self) -> i64 {
+        self.0
+    }
+
+    pub fn y(&self) -> i64 {
+        self.1
+    }
+}
+
+impl std::fmt::Display for Point2D {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {})", self.0, self.1)
+    }
+}
+
+// It's honestly surprising how often you need something like this for AOC...
+#[derive(Clone)]
+pub struct Map2D {
+    filler_tile: u8,
+    rows: Vec<Map2DRow>,
+    y_offset: i64,
+    min_x: i64,
+    max_x: i64,
+}
+
+impl Map2D {
+    pub fn new(filler_tile: u8) -> Self {
+        Self {
+            filler_tile,
+            rows: Vec::new(),
+            y_offset: 0,
+            min_x: 0,
+            max_x: -1,
+        }
+    }
+
+    pub fn from_rows(rows: impl Iterator<Item = Vec<u8>>, filler_tile: u8) -> Self {
+        let mut max_x = -1;
+        let rows = rows.map(|tiles| {
+            max_x = max_x.max(tiles.len() as i64 - 1);
+            Map2DRow::from(tiles)
+        }).collect();
+        Self {
+            filler_tile,
+            rows,
+            y_offset: 0,
+            min_x: 0,
+            max_x,
+        }
+    }
+
+    pub fn min_x(&self) -> i64 {
+        self.min_x
+    }
+
+    pub fn max_x(&self) -> i64 {
+        self.max_x
+    }
+
+    pub fn min_y(&self) -> i64 {
+        self.y_offset
+    }
+
+    pub fn max_y(&self) -> i64 {
+        self.y_offset + self.rows.len() as i64 - 1
+    }
+
+    pub fn get(&self, x: i64, y: i64) -> u8 {
+        self.rows.get((y - self.y_offset) as usize)
+            .map(|row| row.get(x, self.filler_tile))
+            .unwrap_or(self.filler_tile)
+    }
+
+    pub fn get_at(&self, point: Point2D) -> u8 {
+        let Point2D(x, y) = point;
+        self.get(x, y)
+    }
+
+    pub fn put(&mut self, x: i64, y: i64, tile: u8) -> u8 {
+        if self.rows.is_empty() {
+            self.min_x = x;
+            self.max_x = x;
+            self.y_offset = y;
+        }
+        else {
+            self.min_x = self.min_x.min(x);
+            self.max_x = self.max_x.max(x);
+            if y < self.y_offset {
+                let filler_row_iter = std::iter::repeat(Map2DRow::new())
+                    .take((self.y_offset - y) as usize);
+                let mut adjusted_rows = Vec::from_iter(filler_row_iter);
+                adjusted_rows.append(&mut self.rows);
+                self.rows = adjusted_rows;
+                self.y_offset = y;
+            }
+        }
+        if y >= self.y_offset + self.rows.len() as i64 {
+            let filler_row_iter = std::iter::repeat(Map2DRow::new())
+                .take((y - self.y_offset) as usize - self.rows.len() + 1);
+            self.rows.extend(filler_row_iter);
+        }
+        self.rows[(y - self.y_offset) as usize].put(x, tile, self.filler_tile)
+    }
+    
+    pub fn put_at(&mut self, point: Point2D, tile: u8) -> u8 {
+        let Point2D(x, y) = point;
+        self.put(x, y, tile)
+    }
+
+    pub fn clear(&mut self) {
+        self.rows.clear();
+        self.y_offset = 0;
+    }
+}
+
+impl std::fmt::Display for Map2D {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for _ in 0..self.y_offset {
+            writeln!(f)?;
+        }
+        for row in self.rows.iter() {
+            writeln!(f, "{row}")?;
+        }
+        Ok(())
+    }
+}
+
+#[derive(Clone)]
+struct Map2DRow {
+    tiles: Vec<u8>,
+    x_offset: i64,
+}
+
+impl Map2DRow {
+    fn new() -> Self {
+        Self {
+            tiles: Vec::new(),
+            x_offset: 0,
+        }
+    }
+
+    fn get(&self, x: i64, filler_tile: u8) -> u8 {
+        self.tiles.get((x - self.x_offset) as usize)
+            .copied()
+            .unwrap_or(filler_tile)
+    }
+
+    fn put(&mut self, x: i64, tile: u8, filler_tile: u8) -> u8 {
+        if self.tiles.is_empty() {
+            self.x_offset = x;
+        }
+        else if x < self.x_offset {
+            let filler_tile_iter = std::iter::repeat(filler_tile)
+                .take((self.x_offset - x) as usize);
+            let mut adjusted_content = Vec::from_iter(filler_tile_iter);
+            adjusted_content.append(&mut self.tiles);
+            self.tiles = adjusted_content;
+            self.x_offset = x;
+        }
+        if x >= self.x_offset + self.tiles.len() as i64 {
+            let filler_tile_iter = std::iter::repeat(filler_tile)
+                .take((x - self.x_offset) as usize - self.tiles.len() + 1);
+            self.tiles.extend(filler_tile_iter);
+        }
+        std::mem::replace(&mut self.tiles[(x - self.x_offset) as usize], tile)
+    }
+}
+
+impl From<Vec<u8>> for Map2DRow {
+    fn from(tiles: Vec<u8>) -> Self {
+        Self {
+            tiles,
+            x_offset: 0,
+        }
+    }
+}
+
+impl std::fmt::Display for Map2DRow {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{: >2$}{}", "", String::from_utf8_lossy(&self.tiles), self.x_offset as usize)
+    }
+}
